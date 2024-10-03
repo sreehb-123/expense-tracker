7f28ada454b467385eba7c64e124771f804e91d0
sreehb-123 <152023996+sreehb-123@users.noreply.github.com>
2024-10-03 16:00:08 +0530

    minor again

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://expense-tracker-frontend-khe0.onrender.com',
+    methods: ['GET','POST','DELETE'],
+    credentials: true
+}));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI,{
