2ec25cd1ec152c03b32ed5bef3ffff61b45c9f6c
sreehb-123 <152023996+sreehb-123@users.noreply.github.com>
2024-10-04 20:33:20 +0530

    path error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Expenses from './components/Expenses';
 import Login from './components/Login';
 import Home from './components/Home';
-import { Router,Routes,Route } from 'react-router-dom';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
